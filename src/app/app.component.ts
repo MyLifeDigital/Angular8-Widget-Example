66e96b51d80561585f63695e6dbdd88510f2c648
Ciaran Ashton <ciaranashton@me.com>
2020-07-17 15:18:13 +0100

    Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import {
+  Component,
+  Inject,
+  Renderer2,
+} from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    @Inject(DOCUMENT) private _document: Document,
+    private renderer: Renderer2
+  ) { }
+
   title = 'widget8';
+
+  ngOnInit() {
+    const s1 = this.renderer.createElement("script");
+    s1.innerHTML = `
+      var widget_config = {
+        templateId: '< Template Id >',
+        token: '< Token >'
+      }
+    `
+    this.renderer.appendChild(this._document.body, s1);
+
+    const s2 = this.renderer.createElement("script");
+    s2.src = 'https://widget.sandbox.consentric.io/public/init.js';
+    s2.id = "init";
+    this.renderer.appendChild(this._document.body, s2);
+  }
 }
