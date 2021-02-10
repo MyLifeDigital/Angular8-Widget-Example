9b55f05b4c116534a3365e1016a810a3eaceec69
aprovis <aprovis@mylifedigital.co.uk>
2021-02-10 17:09:37 +0000

    Fixed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import {
   Renderer2,
 } from "@angular/core";
 
+declare let StatefulWidget: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -19,18 +21,69 @@ export class AppComponent {
   title = 'widget8';
 
   ngOnInit() {
-    const s1 = this.renderer.createElement("script");
-    s1.innerHTML = `
-      var widget_config = {
-        templateId: '< Template Id >',
-        token: '< Token >'
-      }
-    `
-    this.renderer.appendChild(this._document.body, s1);
+    // const s1 = this.renderer.createElement("script");
+    // s1.innerHTML = `
+    // var widget = StatefulWidget.load({
+    //   id: 'widget-container',
+    //   token: '87b45c32ebc6110eb7eca2555b385c2c',            
+    //   templateId: 'N1kPKeKr7ek',
+    //   display: {
+    //       location: 'inside',
+    //       consentricLogo: false,
+    //       closeOnSubmit: false,
+    //       displayButtons: true,
+    //       displayCancelButtons: false,
+    //       displayAllNoneOptions: true,
+    //       applyDefaultStyle: true,
+    //   },
+    //   events: {
+    //       onSuccess: () => {
+    //           console.log('success');
+    //       },
+    //       onUpdate: (state) => {
+    //           console.log(state);
+    //       },
+    //       onLoadError: (error) => {
+    //           console.log('Error');
+    //           console.log(error);
+    //       }
+    //   }
+    // });
+    // `
+    // this.renderer.appendChild(this._document.body, s1);
 
-    const s2 = this.renderer.createElement("script");
-    s2.src = 'https://widget.sandbox.consentric.io/public/init.js';
-    s2.id = "init";
-    this.renderer.appendChild(this._document.body, s2);
+    var widget = StatefulWidget.load({
+      id: 'widget-container',
+      token: '68f11ef72c3256030c86f3cca531978d',            
+      templateId: 'N1kPKeKr7ek',
+      display: {
+          location: 'inside',
+          consentricLogo: false,
+          closeOnSubmit: false,
+          displayButtons: true,
+          displayCancelButtons: false,
+          displayAllNoneOptions: true,
+          applyDefaultStyle: true,
+      },
+      events: {
+          onSuccess: () => {
+              console.log('success');
+          },
+          onUpdate: (state) => {
+              console.log(state);
+          },
+          onLoadError: (error) => {
+              console.log('Error');
+              console.log(error);
+          }
+      }
+    });
+    
+
+
+    // const s2 = this.renderer.createElement("script");
+    // s2.src = 'https://widget.sandbox.consentric.io/public/init.js';
+    // s2.id = "init";
+    // this.renderer.appendChild(this._document.body, s2);
   }
 }
